b28badc2901f9867890ddad0154b20a7733ca0ac
Tal Katz <52050412+tkmedia@users.noreply.github.com>
2019-06-21 15:12:23 +0300

    Update wordpress-cleanup.php

--- inc/wordpress-cleanup.php
+++ inc/wordpress-cleanup.php
@@ -19,7 +19,7 @@
   * @param  string $url Request URL
   * @return array Amended request arguments
   */
- function tkm_dont_update_theme( $r, $url ) {
+ function tkmulti_dont_update_theme( $r, $url ) {
  	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
   		return $r; // Not a theme update request. Bail immediately.
   	$themes = json_decode( $r['body']['themes'] );
@@ -28,23 +28,23 @@
   	$r['body']['themes'] = json_encode( $themes );
   	return $r;
   }
- add_filter( 'http_request_args', 'tkm_dont_update_theme', 5, 2 );
+ add_filter( 'http_request_args', 'tkmulti_dont_update_theme', 5, 2 );
 /**
  * Dequeue jQuery Migrate
  *
  */
-function tkm_dequeue_jquery_migrate( &$scripts ){
+function tkmulti_dequeue_jquery_migrate( &$scripts ){
 	if( !is_admin() ) {
 		$scripts->remove( 'jquery');
 		$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.10.2' );
 	}
 }
-add_filter( 'wp_default_scripts', 'tkm_dequeue_jquery_migrate' );
+add_filter( 'wp_default_scripts', 'tkmulti_dequeue_jquery_migrate' );
 /**
  * Header Meta Tags
  *
  */
-function tkm_header_meta_tags() {
+function tkmulti_header_meta_tags() {
 	echo '<meta http-equiv="content-type" content="' . get_bloginfo( 'html_type' ) . '">';
 	echo '<meta charset="' . get_bloginfo( 'charset' ) . '">';
 	echo '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
@@ -52,12 +52,12 @@ function tkm_header_meta_tags() {
 	echo '<link rel="profile" href="http://gmpg.org/xfn/11">';
 	echo '<link rel="pingback" href="' . get_bloginfo( 'pingback_url' ) . '">';
 }
-add_action( 'wp_head', 'tkm_header_meta_tags' );
+add_action( 'wp_head', 'tkmulti_header_meta_tags' );
 /**
  * Clean Nav Menu Classes
  *
  */
-function tkm_clean_nav_menu_classes( $classes ) {
+function tkmulti_clean_nav_menu_classes( $classes ) {
 	if( ! is_array( $classes ) )
 		return $classes;
 	foreach( $classes as $i => $class ) {
@@ -83,12 +83,12 @@ function tkm_clean_nav_menu_classes( $classes ) {
 	}
 	return $classes;
 }
-add_filter( 'nav_menu_css_class', 'tkm_clean_nav_menu_classes', 5 );
+add_filter( 'nav_menu_css_class', 'tkmulti_clean_nav_menu_classes', 5 );
 /**
  * Clean Post Classes
  *
  */
-function tkm_clean_post_classes( $classes ) {
+function tkmulti_clean_post_classes( $classes ) {
 	if( ! is_array( $classes ) )
 		return $classes;
 	// Change hentry to entry, remove if adding microformat support
@@ -101,36 +101,36 @@ function tkm_clean_post_classes( $classes ) {
    	);
 	return array_intersect( $classes, $allowed_classes );
 }
-add_filter( 'post_class', 'tkm_clean_post_classes', 5 );
+add_filter( 'post_class', 'tkmulti_clean_post_classes', 5 );
 /**
  * Staff comment class
  *
  */
-function tkm_staff_comment_class( $classes, $class, $comment_id, $comment, $post_id ) {
+function tkmulti_staff_comment_class( $classes, $class, $comment_id, $comment, $post_id ) {
 	if( empty( $comment->user_id ) )
 		return $classes;
 	$staff_roles = array( 'comment_manager', 'author', 'editor', 'administrator' );
-	$staff_roles = apply_filters( 'tkm_staff_roles', $staff_roles );
+	$staff_roles = apply_filters( 'tkmulti_staff_roles', $staff_roles );
 	$user = get_userdata( $comment->user_id );
 	if( !empty( array_intersect( $user->roles, $staff_roles ) ) )
 		$classes[] = 'staff';
 	return $classes;
 }
-add_filter( 'comment_class', 'tkm_staff_comment_class', 10, 5 );
+add_filter( 'comment_class', 'tkmulti_staff_comment_class', 10, 5 );
 /**
  * Remove avatars from comment list
  *
  */
-function tkm_remove_avatars_from_comments( $avatar ) {
+function tkmulti_remove_avatars_from_comments( $avatar ) {
 	global $in_comment_loop;
 	return $in_comment_loop ? '' : $avatar;
 }
-add_filter( 'get_avatar', 'tkm_remove_avatars_from_comments' );
+add_filter( 'get_avatar', 'tkmulti_remove_avatars_from_comments' );
 /**
  * Archive Title, remove prefix
  *
  */
-function tkm_archive_title_remove_prefix( $title ) {
+function tkmulti_archive_title_remove_prefix( $title ) {
 	$title_pieces = explode( ': ', $title );
 	if( count( $title_pieces ) > 1 ) {
 		unset( $title_pieces[0] );
@@ -138,24 +138,24 @@ function tkm_archive_title_remove_prefix( $title ) {
 	}
 	return $title;
 }
-add_filter( 'get_the_archive_title', 'tkm_archive_title_remove_prefix' );
+add_filter( 'get_the_archive_title', 'tkmulti_archive_title_remove_prefix' );
 /**
  * Comment form, button class
  *
  */
-function tkm_comment_form_button_class( $args ) {
+function tkmulti_comment_form_button_class( $args ) {
 	$args['class_submit'] = 'submit wp-block-button__link';
 	return $args;
 }
-add_filter( 'comment_form_defaults', 'tkm_comment_form_button_class' );
+add_filter( 'comment_form_defaults', 'tkmulti_comment_form_button_class' );
 /**
  * Excerpt More
  *
  */
-function tkm_excerpt_more() {
+function tkmulti_excerpt_more() {
 	return '&hellip;';
 }
-add_filter( 'excerpt_more', 'tkm_excerpt_more' );
+add_filter( 'excerpt_more', 'tkmulti_excerpt_more' );
 /**
  * Posts Navigation as Buttons
  * @author Bill Erickson
