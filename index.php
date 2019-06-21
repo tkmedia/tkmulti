b5d675731447b62b6843fa248fe06bd909d20635
Tal Katz <tal@tkmedia.co.il>
2019-06-22 01:01:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,21 +7,17 @@
  * @since        1.0.0
  * @license      GPL-2.0+
 **/
-
 get_header();
-
 tha_content_before();
-
-	echo '<div class="' . ea_class( 'content-area', 'wrap', apply_filters( 'ea_content_area_wrap', true ) ) . '">';
+	echo '<div class="' . tkmulti_class( 'content-area', 'wrap', apply_filters( 'ea_content_area_wrap', true ) ) . '">';
 	tha_content_wrap_before();
 	echo '<main class="site-main" role="main">';
 	tha_content_top();
 	tha_content_loop();
 	tha_content_bottom();
 	echo '</main>';
-	get_sidebar();
+	//get_sidebar();
 	tha_content_wrap_after();
 	echo '</div>';
 tha_content_after();
-
 get_footer();
