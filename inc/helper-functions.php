10f272b010bec801b604cdbcd23aa8bc237aa842
Tal Katz <52050412+tkmedia@users.noreply.github.com>
2019-06-21 15:26:07 +0300

    Update helper-functions.php

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -2,11 +2,12 @@
 /**
  * Helper Functions
  *
- * @package      TKMulti
- * @author       Tal Katz TKMedia.co.il
+ * @package      EAStarter
+ * @author       Bill Erickson
  * @since        1.0.0
  * @license      GPL-2.0+
 **/
+
 // Duplicate 'the_content' filters
 global $wp_embed;
 add_filter( 'tkmulti_the_content', array( $wp_embed, 'run_shortcode' ), 8 );
@@ -16,6 +17,7 @@ add_filter( 'tkmulti_the_content', 'convert_chars'      );
 add_filter( 'tkmulti_the_content', 'wpautop'            );
 add_filter( 'tkmulti_the_content', 'shortcode_unautop'  );
 add_filter( 'tkmulti_the_content', 'do_shortcode'       );
+
 /**
  * Get the first term attached to post
  *
@@ -25,23 +27,31 @@ add_filter( 'tkmulti_the_content', 'do_shortcode'       );
  * @return string/object
  */
 function tkmulti_first_term( $taxonomy = 'category', $field = false, $post_id = false ) {
+
 	$post_id = $post_id ? $post_id : get_the_ID();
 	$term = false;
+
 	// Use WP SEO Primary Term
 	// from https://github.com/Yoast/wordpress-seo/issues/4038
 	if( class_exists( 'WPSEO_Primary_Term' ) ) {
 		$term = get_term( ( new WPSEO_Primary_Term( $taxonomy,  $post_id ) )->get_primary_term(), $taxonomy );
 	}
+
 	// Fallback on term with highest post count
 	if( ! $term || is_wp_error( $term ) ) {
+
 		$terms = get_the_terms( $post_id, $taxonomy );
+
 		if( empty( $terms ) || is_wp_error( $terms ) )
 			return false;
+
 		// If there's only one term, use that
 		if( 1 == count( $terms ) ) {
 			$term = array_shift( $terms );
+
 		// If there's more than one...
 		} else {
+
 			// Sort by term order if available
 			// @uses WP Term Order plugin
 			if( isset( $terms[0]->order ) ) {
@@ -49,6 +59,7 @@ function tkmulti_first_term( $taxonomy = 'category', $field = false, $post_id = 
 				foreach( $terms as $term )
 					$list[$term->order] = $term;
 				ksort( $list, SORT_NUMERIC );
+
 			// Or sort by post count
 			} else {
 				$list = array();
@@ -57,15 +68,19 @@ function tkmulti_first_term( $taxonomy = 'category', $field = false, $post_id = 
 				ksort( $list, SORT_NUMERIC );
 				$list = array_reverse( $list );
 			}
+
 			$term = array_shift( $list );
 		}
 	}
+
 	// Output
 	if( $field && isset( $term->$field ) )
 		return $term->$field;
+
 	else
 		return $term;
 }
+
 /**
  * Conditional CSS Classes
  *
@@ -77,6 +92,7 @@ function tkmulti_first_term( $taxonomy = 'category', $field = false, $post_id = 
 function tkmulti_class( $base_classes, $optional_class, $conditional ) {
 	return $conditional ? $base_classes . ' ' . $optional_class : $base_classes;
 }
+
 /**
  * Column Classes
  *
@@ -89,11 +105,14 @@ function tkmulti_class( $base_classes, $optional_class, $conditional ) {
  * @return string/array $classes
  */
 function tkmulti_column_class( $classes = array(), $current = false, $join = true ) {
+
 	if( false === $current )
 		return $classes;
+
 	$columns = array( 2, 3, 4, 6 );
 	foreach( $columns as $column ) {
 		if( 0 == $current % $column ) {
+
 			$col = 12 / $column;
 			foreach( $classes as $class ) {
 				if( false != strstr( $class, (string) $col ) && false == strstr( $class, '12' ) ) {
@@ -102,12 +121,14 @@ function tkmulti_column_class( $classes = array(), $current = false, $join = tru
 			}
 		}
 	}
+
 	if( $join ) {
 		return join( ' ', $classes );
 	} else {
 		return $classes;
 	}
 }
+
 /**
  *  Background Image Style
  *
@@ -118,6 +139,7 @@ function tkmulti_bg_image_style( $image_id = false, $image_size = 'full' ) {
 	if( !empty( $image_id ) )
 		return ' style="background-image: url(' . wp_get_attachment_image_url( $image_id, $image_size ) . ');"';
 }
+
 /**
  * Get Icon
  * This function is in charge of displaying SVG icons across the site.
@@ -131,6 +153,7 @@ function tkmulti_bg_image_style( $image_id = false, $image_size = 'full' ) {
  *
  */
 function tkmulti_icon( $atts = array() ) {
+
 	$atts = shortcode_atts( array(
 		'icon'	=> false,
 		'group'	=> 'utility',
@@ -138,23 +161,31 @@ function tkmulti_icon( $atts = array() ) {
 		'class'	=> false,
 		'title'	=> false,
 	), $atts );
+
 	if( empty( $atts['icon'] ) )
 		return;
+
 	$icon_path = get_template_directory() . '/assets/icons/' . $atts['group'] . '/' . $atts['icon'] . '.svg';
 	if( ! file_exists( $icon_path ) )
 		return;
+
 	$icon = file_get_contents( $icon_path );
+
 	$class = 'svg-icon';
 	if( !empty( $atts['class'] ) )
 		$class .= ' ' . esc_attr( $atts['class'] );
+
 	$repl = sprintf( '<svg class="' . $class . '" width="%d" height="%d" aria-hidden="true" role="img" focusable="false" ', $atts['size'], $atts['size'] );
 	$svg  = preg_replace( '/^<svg /', $repl, trim( $icon ) ); // Add extra attributes to SVG code.
 	$svg  = preg_replace( "/([\n\t]+)/", ' ', $svg ); // Remove newlines & tabs.
 	$svg  = preg_replace( '/>\s*</', '><', $svg ); // Remove white space between SVG tags.
+
 	if( !empty( $atts['title'] ) )
 		$svg .= '<span class="screen-reader-text">' . esc_html( $atts['title'] ) . '</span>';
+
 	return $svg;
 }
+
 /**
  * Has Action
  *
