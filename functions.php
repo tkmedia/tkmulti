5ec807f80bcff5acd5e94dc5705254ab41197ac0
Tal Katz <tal@tkmedia.co.il>
2019-06-22 23:11:13 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Functions and Definitions
+ * Header functions.
  *
  * @package      TKMulti
  * @author       Tal Katz TKMedia.co.il
@@ -9,64 +9,252 @@
  * @link http://www.tkmedia.co.il
 **/
 
-define( 'THEME_DIR', get_template_directory() );
-define( 'THEME_URI', get_template_directory_uri() );
+/* ---------------------------------------------------------------------------
+ * Theme support
+ * --------------------------------------------------------------------------- */
 
-define( 'THEME_NAME', 'tkmulti' );
-define( 'THEME_VERSION', '1.0.1' );
+if( ! function_exists( 'tkmulti_setup' ) )
+{
 
-define( 'LIBS_DIR', THEME_DIR. '/inc/functions' );
-define( 'LIBS_URI', THEME_URI. '/inc/functions' );
-define( 'LANG_DIR', THEME_DIR. '/languages' );
+	function tkmulti_setup() {
 
-/* ----------------------------------------------------------------------------
- * Loads Theme Textdomain
- * ---------------------------------------------------------------------------- */
-// Make theme available for translation.
-// ranslations can be placed in the /languages/ directory.
-load_theme_textdomain( THEME_NAME,  LANG_DIR );	// frontend
-load_theme_textdomain( 'tkm-opts', LANG_DIR );	// backend
+		if( false ) add_editor_style( '/css/style-login.css' );
 
-/* ----------------------------------------------------------------------------
- * Loads Theme Functions
- * ---------------------------------------------------------------------------- */
+		// Add default posts and comments RSS feed links to head
+		add_theme_support( 'automatic-feed-links' );
 
-// Functions ------------------------------------------------------------------
-require( LIBS_DIR .'/tha-theme-hooks.php' );
-require( LIBS_DIR .'/wordpress-cleanup.php' );
-require( LIBS_DIR .'/helper-functions.php' );
+		//add_theme_support( 'menus' );
 
-require_once( LIBS_DIR .'/theme-functions.php' );
+		// Enable support for Post Formats.
+		add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link', 'gallery', 'audio' ) );
 
-// Header ---------------------------------------------------------------------
-require_once( LIBS_DIR .'/theme-head.php' );
+		// Enable support for post thumbnails and featured images.
+		add_theme_support( 'post-thumbnails' );
 
-// ACF Fields -----------------------------------------------------------------
-require_once( THEME_DIR .'/inc/acf-blocks-site.php' );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-// Custom template tags for this theme ----------------------------------------
-//require( LIBS_DIR .'/template-tags.php' );
+		// Enable support for wide Gutenberg images.
+		add_theme_support( 'align-wide' );
 
-// Custom functions that act independently of the theme templates -------------
-//require( LIBS_DIR .'/tweaks.php' );
+		// Enable support for Gutenberg Backend Styles.
+		add_theme_support('editor-styles');
 
-// Include our Theme Customizer code. -----------------------------------------
-require( THEME_DIR .'/inc/customizer.php' );
+		// Admin Bar Styling
+		add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
 
-// Functions Parts ------------------------------------------------------------
-require( THEME_DIR .'/inc/login-logo.php' );
-require( THEME_DIR .'/inc/navigation.php' );
-require( THEME_DIR .'/inc/loop.php' );
-require( THEME_DIR .'/inc/tinymce.php' );
-require( THEME_DIR .'/inc/disable-editor.php' );
-require( THEME_DIR .'/inc/amp.php' );
-require( THEME_DIR .'/inc/display-posts.php' );
-require( THEME_DIR .'/inc/instantpage.php' );
+		//Set the content width in pixels, based on the theme's design and stylesheet.
+		$GLOBALS['content_width'] = apply_filters( 'tkmulti_content_width', 1200 );
 
-// WooCommerce ----------------------------------------------------------------
-if ( class_exists( 'WooCommerce' ) ) {
-	require_once( THEME_DIR . '/inc/woocommerce.php' );
+		// Switch default core markup for search form, comment form, and comments
+		// to output valid HTML5.
+		add_theme_support( 'html5', array( 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		// Body open hook
+		add_theme_support( 'body-open' );
+
+		// Add support for custom navigation menus.
+		register_nav_menus( array(
+		    'primary'   => __( 'Primary menu', 'tkmulti' ),
+		    //'primary-mobile'    => __( 'Primary mobile menu', 'tkmulti' ),
+		    //'secondary'    => __( 'Secondary menu', 'tkmulti' ),
+		    'footer-nav'    => __( 'Footer nav', 'tkmulti' ),
+		    //'footer-nav-left'    => __( 'Footer nav left', 'tkmulti' ),
+		    'panel-nav'    => __( 'Top panel nav', 'tkmulti' )
+		) );
+
+		add_theme_support('custom-logo');
+		$defaults = array(
+		    'height'      => 100,
+		    'width'       => 400,
+		    'flex-height' => true,
+		    'flex-width'  => true,
+		    'header-text' => array( 'site-title', 'site-description' ),
+		);
+		add_theme_support( 'custom-logo', $defaults );
+
+		// Add support for background color and images
+		add_theme_support( 'custom-background', array(
+			'default-color' => 'fff',
+		) );
+
+		// Gutenberg
+
+		// -- Responsive embeds
+		add_theme_support( 'responsive-embeds' );
+
+		// -- Wide Images
+		add_theme_support( 'align-wide' );
+
+		// -- Disable custom font sizes
+		add_theme_support( 'disable-custom-font-sizes' );
+
+		// -- Editor Font Styles
+		add_theme_support( 'editor-font-sizes', array(
+			array(
+				'name'      => __( 'small', 'tkmulti' ),
+				'shortName' => __( 'S', 'tkmulti' ),
+				'size'      => 12,
+				'slug'      => 'small'
+			),
+			array(
+				'name'      => __( 'regular', 'tkmulti' ),
+				'shortName' => __( 'M', 'tkmulti' ),
+				'size'      => 16,
+				'slug'      => 'regular'
+			),
+			array(
+				'name'      => __( 'large', 'tkmulti' ),
+				'shortName' => __( 'L', 'tkmulti' ),
+				'size'      => 20,
+				'slug'      => 'large'
+			),
+		) );
+
+		// -- Disable Custom Colors
+		add_theme_support( 'disable-custom-colors' );
+
+		// -- Editor Color Palette
+		add_theme_support( 'editor-color-palette', array(
+			array(
+				'name'  => __( 'Blue', 'tkmulti' ),
+				'slug'  => 'blue',
+				'color'	=> '#59BACC',
+			),
+			array(
+				'name'  => __( 'Green', 'tkmulti' ),
+				'slug'  => 'green',
+				'color' => '#58AD69',
+			),
+			array(
+				'name'  => __( 'Orange', 'tkmulti' ),
+				'slug'  => 'orange',
+				'color' => '#FFBC49',
+			),
+			array(
+				'name'	=> __( 'Red', 'tkmulti' ),
+				'slug'	=> 'red',
+				'color'	=> '#E2574C',
+			),
+		) );
+
+	}
+
+}
+add_action( 'after_setup_theme', 'tkmulti_setup' );
+
+/* ---------------------------------------------------------------------------
+ * Styles
+ * --------------------------------------------------------------------------- */
+if( ! function_exists( 'tkmulti_styles' ) )
+{
+	function tkmulti_styles()
+	{
+		// wp_enqueue_style ------------------------------------------------------
+		wp_enqueue_style( 'plugins',			THEME_URI .'/css/plugins.css', false, THEME_VERSION );
+		wp_enqueue_style( 'fa5-all',			THEME_URI .'/css/fa5-all.min.css', false, THEME_VERSION );
+		wp_enqueue_style( 'v4-shims',			THEME_URI .'/css/v4-shims.min.css', false, THEME_VERSION );
+		wp_enqueue_style( 'global',		    	THEME_URI .'/css/global.css', false, THEME_VERSION );
+		wp_enqueue_style( 'aos',	            THEME_URI .'/css/aos.css', false, THEME_VERSION );
+		wp_enqueue_style( 'fancybox',	        THEME_URI .'/css/jquery.fancybox.min.css', false, THEME_VERSION );
+		wp_enqueue_style( 'main-style',	        THEME_URI .'/css/main.css', false, THEME_VERSION );
+
+		// Google Fonts ----------------------------------------------------------
+		wp_enqueue_style( 'tkmulti-fonts', tkmulti_theme_fonts_url() );
+
+	}
+}
+add_action( 'wp_enqueue_scripts', 'tkmulti_styles' );
+
+//ENQUEUE BACKEND RESOURCES
+function load_admin_style() {
+	wp_enqueue_style( 'admin_css', THEME_URI . '/assets/css/style-login.css', false, THEME_VERSION );
+}
+add_action( 'admin_enqueue_scripts', 'load_admin_style' );
+
+
+/* ---------------------------------------------------------------------------
+ * Scripts
+ * --------------------------------------------------------------------------- */
+if( ! function_exists( 'tkmulti_scripts' ) )
+{
+	function tkmulti_scripts()
+	{
+		// Custom ----------------------------------
+		wp_enqueue_script( 'plugins', THEME_URI .'/js/plugins.js', array( 'jquery' ), THEME_VERSION, true );
+		wp_enqueue_script( 'gmapsapi', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD6FEYaLhW3OhWJhHovx0BK3MAxtkGSAMw&language=he', array(), THEME_VERSION, true );
+		wp_enqueue_script( 'acfmaps', THEME_URI . '/js/acfmaps.js', array( 'jquery' ), THEME_VERSION, true );
+		wp_enqueue_script( 'simpleParallax', THEME_URI . '/js/simpleParallax.min.js', array( 'jquery' ), '', true );
+		wp_enqueue_script( 'fancybox', THEME_URI . '/js/jquery.fancybox.min.js', array( 'jquery' ), '', true );
+		wp_enqueue_script( 'aos', THEME_URI . '/js/aos.js', array( 'jquery' ), '', true );
+
+		// Main config -----------------------------
+		wp_enqueue_script( 'functionality', THEME_URI .'/js/functionality.js', array( 'jquery' ), THEME_VERSION, true );
+		wp_enqueue_script( 'essentials', THEME_URI .'/js/essentials.js', array( 'jquery' ), THEME_VERSION, true );
+		wp_enqueue_script( 'main', THEME_URI .'/js/main.js', array( 'jquery' ), THEME_VERSION, true );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'tkmulti_scripts' );
+
+function fontawesome_add_defer_attribute($tag, $handle) {
+	if ( 'fontawesome' !== $handle )
+	return $tag;
+	return str_replace( ' src', ' defer src', $tag );
+}
+add_filter('script_loader_tag', 'fontawesome_add_defer_attribute', 10, 2);
+
+/* ---------------------------------------------------------------------------
+ * WooCommerce
+ * --------------------------------------------------------------------------- */
+ //unhook the WooCommerce wrappers:
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+//hook in your own functions to display the wrappers your theme requires:
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<section id="main_wc" class="main_wc clearfix">';
 }
 
-add_filter( 'get_comment_date', '__return_empty_string' );
-add_filter( 'get_comment_time', '__return_empty_string' );
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+/* ---------------------------------------------------------------------------
+ * Theme Fonts URL
+ * --------------------------------------------------------------------------- */
+function tkmulti_theme_fonts_url() {
+	$font_families = apply_filters( 'tkmulti_theme_fonts', array( 'Assistant:200,400,600,700' ) );
+	$query_args = array(
+		'family' => implode( '|', $font_families ),
+		'subset' => 'hebrew,latin',
+	);
+	$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	return esc_url_raw( $fonts_url );
+}
+
+/* ---------------------------------------------------------------------------
+ * Template Hierarchy
+ * --------------------------------------------------------------------------- */
+function tkmulti_template_hierarchy( $template ) {
+
+	if( is_home() || is_search() )
+		$template = get_query_template( 'archive' );
+	return $template;
+}
+add_filter( 'template_include', 'tkmulti_template_hierarchy' );
