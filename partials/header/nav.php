8cfcaaf488badd6d0a5aa3c9c77f7930fb5f4e07
Tal Katz <tal@tkmedia.co.il>
2019-06-24 14:23:41 +0300

    Update nav.php

--- partials/header/nav.php
+++ partials/header/nav.php
@@ -1,11 +1,9 @@
 <div class="header_menu site-header clearfix dt-mobile-header">
 	<div id="header-menu-wrapper" class="page-header-menu">
 		<div id="header-menu-wrapper-inner" class="menu-container container clearfix">
-			<nav id="header-menu"  class="nav nav-primary menu collapse navbar-collapse collapsed" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation" aria-label="<?php _e( 'תפריט ראשי', 'tkmulti' ); ?>">
+			<nav id="header-menu"  class="nav nav-primary menu collapse navbar-collapse collapsed" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation" aria-label="<?php _e( 'Main Nav', 'tkmulti' ); ?>">
 			<input id="main-menu-state" type="checkbox" />
-			<?php
-			if ( !wp_is_mobile() ) {	
-				
+			<?php				
 				wp_nav_menu( array (
 						'theme_location'    => 'primary',
 						'depth'             => 3,
@@ -19,23 +17,6 @@
 					)
 				);
 				
-			} else {
-				
-				wp_nav_menu( array (
-						'theme_location'    => 'primary-mobile',
-						'depth'             => 3,
-						'container'         => '',
-						'container_id'      => '',
-						'container_class'   => '',
-						'menu_id'      => 'main-menu',
-						'menu_class'        => 'header-main-menu main-navigation',
-						//'items_wrap'     => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>',
-						'walker'         => new dynamicWalkerNavMenu(),
-					)
-				);
-			
-			}
-			
 			?>						
 		</div>
 	</div>
