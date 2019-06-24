63782645fd1bd85ab37d63c332a464394b16438d
Tal Katz <tal@tkmedia.co.il>
2019-06-24 11:46:12 +0300

    Create hamburger.php

---
+++ partials/header/hamburger.php
@@ -0,0 +1,10 @@
+<div id="open-button" class="hamburger menu-button dt-mobile-menu-icon hamburger--spin" aria-label="Menu" aria-controls="navigation" aria-expanded="true">
+	<div class="hamburger_mod">
+	  <div class="hamburger-menu">
+	    <div class="bar"></div>
+	    <div class="bar"></div>
+	    <div class="bar"> </div>
+	  </div>
+	</div>
+</div>
+<div class="mobile-sticky-header-overlay"></div>
