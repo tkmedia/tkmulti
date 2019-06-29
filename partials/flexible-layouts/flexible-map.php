c85b3b80e0708328b1d6a2fec3121e2dab777d9e
Tal Katz <tal@tkmedia.co.il>
2019-06-29 10:38:27 +0300

    Update flexible-map.php

--- partials/flexible-layouts/flexible-map.php
+++ partials/flexible-layouts/flexible-map.php
@@ -3,6 +3,8 @@ $flex_map_block_width = get_sub_field('flex_map_block_width');
 $flex_map_order = get_sub_field('flex_map_order');
 $flex_map_mobile = get_sub_field('flex_map_mobile');
 $flex_map_hide_mobile = get_sub_field('flex_map_hide_mobile');
+$flex_map_break = get_sub_field('flex_map_break');
+$flex_map_block_align = get_sub_field('flex_map_block_align');
 
 $map = get_sub_field('flex_map');
 $map_name = get_sub_field('flex_map_name');
@@ -12,7 +14,7 @@ if ( $flex_map_hide_mobile && wp_is_mobile() ) {
 //HIDE ON MOBILE
 } else { ?>
 
-<div class="flex_content_cols <?php echo $flex_map_mobile;?> <?php echo $flex_map_block_width;?>" <?php if( $flex_map_order ){ ?>style="order:<?php echo $flex_map_order; ?>;"<?php } ?>>
+<div class="flex_content_cols <?php echo $flex_map_mobile;?> <?php echo $flex_map_block_width;?> <?php if( $flex_map_break ){ ?><?php echo $flex_map_block_align; ?><?php } ?>" <?php if( $flex_map_order ){ ?>style="order:<?php echo $flex_map_order; ?>;"<?php } ?>>
 	<section id="section-<?php echo $row;?>-<?php echo $count;?>" class="page_flexible page_flexible_content section-<?php echo $row;?>-<?php echo $count;?> count_sections_<?php echo $count;?>" data-aos="<?php echo $map_animation;?>">
 
 		<div class="flex_map_container flexible_page_element" itemprop="text">
@@ -35,4 +37,5 @@ if ( $flex_map_hide_mobile && wp_is_mobile() ) {
 		
 	</section>
 </div>
+<?php if( $flex_map_break ){ ?><div class="break"></div><?php } ?>
 <?php } ?>
