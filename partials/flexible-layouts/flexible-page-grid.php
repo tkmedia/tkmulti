4e4921597dcd6d499d376dd6920d6924a95fe20e
Tal Katz <tal@tkmedia.co.il>
2019-07-03 16:10:22 +0300

    Update flexible-page-grid.php

--- partials/flexible-layouts/flexible-page-grid.php
+++ partials/flexible-layouts/flexible-page-grid.php
@@ -46,8 +46,11 @@ if ( $article_grid_hide_mobile && wp_is_mobile() ) {
 				<?php $item = 1; foreach( $flex_article_grid as $post ): ?>
 				<?php setup_postdata($post); $item++; ?>                     
 			    
+				    <?php if( $article_grid_style == 'style1' || $article_grid_style == 'style2' ){ ?> 
 				    <div class="page_link_grid_item articles_grid_item item-<?php echo $item;?> col-xs-<?= $ag_xs_cols; ?> col-sm-<?= $ag_sm_cols; ?> col-md-<?= $ag_md_cols; ?>">
-					    
+					<?php } else { ?>    
+				    <div class="page_link_grid_item articles_grid_item item-<?php echo $item;?> col-xs-12 col-sm-6 col-md-6">
+					<?php } ?>    
 					<?php if( $article_grid_style == 'style1' ){ ?>    
 						<div class="articles_grid_item_container title_<?php echo $article_grid_title_position;?>">
 							<div class="articles_grid_item_img box_effect">
