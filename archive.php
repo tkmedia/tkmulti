f090d09a6e8d25d8726e0f2e4934df3e705bbebc
tkmedia <tal@tkmedia.co.il>
2019-07-04 17:33:14 +0300

    Update archive.php

--- archive.php
+++ archive.php
@@ -23,120 +23,162 @@ function theme_add_body_class( $classes ) {
  *
  */
  ?> 
-<div id="home_masthead" itemprop="text">	
-	<div class="home_masthead intro-section masthead_clean">	
-	<!-- Top Slider -->
-		<div id="main-top-slider">
-		
-			<?php 
-			$slider_images = get_field('page_main_top_slider');
-			$default_masthead_bg =  get_field('default_main_masthead_bg', 'option');?>
-			<div class="top-slider-bg top-slider-bg-multiple">
-			    <div id="top-slider" class="swiper-container style3 swiper-scale-effect" style="direction: ltr;height: 30vh !important;">
-				    <?php if( $slider_images ) { ?>
-			        <div class="slides single-slider swiper-wrapper">
-			            <?php foreach( $slider_images as $slider_image ): ?>
-				        <div class="single-slider-img-item single-slider-item swiper-slide">
-			                <div class="single-slider-img swiper-slide-cover">
-				                <?php //echo wp_get_attachment_image( $slider_image['ID'], 'full' ); ?>
-								<div class="slide-inner" style="background-image:url(<?php echo $slider_image['url']; ?>)"></div>
-			                </div>
-				        </div>
-			            <?php endforeach; ?>
-			        <?php } elseif ($default_masthead_bg) { ?>
-			        <div class="slides single-slider">
-				        <div class="single-slider-img single-slider-item">
-					        <div class="single-slider-img"><?php echo wp_get_attachment_image( $default_masthead_bg, 'full' ); ?></div>
-				        </div>
-			        <?php } ?>
-			        </div>			        
-			    </div>
+ 
+<?php get_header(); ?>
 
-				<div class="masthead_clean_content">
-					<div class="masthead_content wrap row-flex bottom-xs between-xs center-xs">
-						<div class="masthead_content_container col-xs-12 col-md">
-							<div class="masthead_content_container_wrap">
-								<?php								
-								//function tkmulti_archive_header() {
-									$title = $subtitle = $description = false;
-									if( is_home() ) {
-										$title = get_the_title( get_option( 'page_for_posts' ) );
-									} elseif( is_search() ) {
-										$title = 'Search Results for: <em>' . get_search_query() . '</em>';
-									} elseif( is_archive() ) {
-										$title = get_the_archive_title();
-										$description = get_the_archive_description();
-									} elseif( ! is_post_type_archive() ) {
-										$title = single_cat_title();
-									} else {
-										$title = post_type_archive_title();
-									}
+<div id="archive_main_content">
+	<div class="page-container">	
+		<div class="entry-content" itemprop="text">
+			
+			
+			<div id="home_masthead" itemprop="text">	
+				<div class="home_masthead intro-section masthead_clean">	
+				<!-- Top Slider -->
+					<div id="main-top-slider">
+					
+						<?php 
+						$slider_images = get_field('page_main_top_slider');
+						$default_masthead_bg =  get_field('default_main_masthead_bg', 'option');?>
+						<div class="top-slider-bg top-slider-bg-multiple">
+						    <div id="top-slider" class="swiper-container style3 swiper-scale-effect" style="direction: ltr;height: 30vh !important;">
+							    <?php if( $slider_images ) { ?>
+						        <div class="slides single-slider swiper-wrapper">
+						            <?php foreach( $slider_images as $slider_image ): ?>
+							        <div class="single-slider-img-item single-slider-item swiper-slide">
+						                <div class="single-slider-img swiper-slide-cover">
+							                <?php //echo wp_get_attachment_image( $slider_image['ID'], 'full' ); ?>
+											<div class="slide-inner" style="background-image:url(<?php echo $slider_image['url']; ?>)"></div>
+						                </div>
+							        </div>
+						            <?php endforeach; ?>
+						        <?php } elseif ($default_masthead_bg) { ?>
+						        <div class="slides single-slider">
+							        <div class="single-slider-img single-slider-item">
+								        <div class="single-slider-img"><?php echo wp_get_attachment_image( $default_masthead_bg, 'full' ); ?></div>
+							        </div>
+						        <?php } ?>
+						        </div>			        
+						    </div>
+			
+							<div class="masthead_clean_content">
+								<div class="masthead_content wrap row-flex bottom-xs between-xs center-xs">
+									<div class="masthead_content_container col-xs-12 col-md">
+										<div class="masthead_content_container_wrap">
+											<?php								
+											//function tkmulti_archive_header() {
+												$title = $subtitle = $description = false;
+												if( is_home() ) {
+													$title = get_the_title( get_option( 'page_for_posts' ) );
+												} elseif( is_search() ) {
+													$title = 'Search Results for: <em>' . get_search_query() . '</em>';
+												} elseif( is_archive() ) {
+													$title = get_the_archive_title();
+													$description = get_the_archive_description();
+												} elseif( ! is_post_type_archive() ) {
+													$title = single_cat_title();
+												} else {
+													$title = post_type_archive_title();
+												}
+												
+												if( empty( $title ) && empty( $description ) )
+													return;
+												if( get_query_var( 'paged' ) ) {
+													global $wp_query;
+													$subtitle = 'Page ' . get_query_var( 'paged' ) . ' of ' . $wp_query->max_num_pages;
+												}
+												if( ! empty( $title ) )
+													echo '<h1 class="entry-title masthead_content_title archive-title" itemprop="headline">' . $title . '</h1>';
+												if( !empty( $subtitle ) )
+													echo '<h4>' . $subtitle . '</h4>';
+											//}
+											//add_action( 'tha_content_while_before', 'tkmulti_archive_header' );
+											?>								
+										</div>	
+									</div>
 									
-									if( empty( $title ) && empty( $description ) )
-										return;
-									if( get_query_var( 'paged' ) ) {
-										global $wp_query;
-										$subtitle = 'Page ' . get_query_var( 'paged' ) . ' of ' . $wp_query->max_num_pages;
-									}
-									if( ! empty( $title ) )
-										echo '<h1 class="entry-title masthead_content_title archive-title" itemprop="headline">' . $title . '</h1>';
-									if( !empty( $subtitle ) )
-										echo '<h4>' . $subtitle . '</h4>';
-								//}
-								//add_action( 'tha_content_while_before', 'tkmulti_archive_header' );
-								?>								
-							</div>	
-						</div>
+								</div>
+							</div>
+			
+						    <!-- Add Arrows -->
+						    <div id="js-pagevertical1" class="swiper-pagination style1"></div>
+						    <div id="js-next1" class="swiper-button-next"></div>
+						    <div id="js-prev1" class="swiper-button-prev"></div>
+			
+						</div>	
 						
 					</div>
+					<?php if ( !is_front_page() ) { ?>
+					<div class="yoast_breadcrumb col-xs-12 col-md">
+						<div class="yoast_breadcrumb_wrap wrap">
+						<?php if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<div id="breadcrumbs">','</div>');} ?>				
+						</div>
+					</div>
+					<?php } ?>
+					
 				</div>
-
-			    <!-- Add Arrows -->
-			    <div id="js-pagevertical1" class="swiper-pagination style1"></div>
-			    <div id="js-next1" class="swiper-button-next"></div>
-			    <div id="js-prev1" class="swiper-button-prev"></div>
-
-			</div>	
-			
-		</div>
-		<?php if ( !is_front_page() ) { ?>
-		<div class="yoast_breadcrumb col-xs-12 col-md">
-			<div class="yoast_breadcrumb_wrap wrap">
-			<?php if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<div id="breadcrumbs">','</div>');} ?>				
+				
 			</div>
-		</div>
-		<?php } ?>
-		
-	</div>
+			<?php 
+			$description = get_the_archive_description();
+			if( ! empty( $description ) ) { ?>
+			<div class="masthead_clean_intro">
+				<?php echo '<div class="home_masthead_text wrap archive-description">' . apply_filters( 'tkmulti_the_content', $description ) . '</div>'; ?>
+			</div>
+			<?php } ?>
+			<?php 
+			// Build the page
+			require get_template_directory() . '/index.php';
+			 ?>
+			<script>
+			jQuery(function($) {
+			
+				// Auto Padding content top
+				$(window).load(function(){
+					get_header_height();
+				    //function to get current div height
+				    function get_header_height(){
+				        //var footer_height = $('#footer_container').height();
+				        var header_height = $('.block_header #header-container').outerHeight();
+				        topSlider = $("#home_masthead #top-slider .slides");
+				        topSliderImg = $("#home_masthead #top-slider .single-slider-img");
+				        topSlider.css('height', "calc(100vh - " + header_height + "px)");
+				        topSliderImg.css('height', "calc(100vh - " + header_height + "px)");
+				    }
+			    });	
+			
+			});
+			</script>  
+			
+			<div class="magazine_page_index wrap">
+				<div class="magazine_page_grid row-flex articles_grid_item_row center-xs">
+					
+				    <?php if ( have_posts() ) : ?>
+				    
+				        <?php while ( have_posts() ) : the_post(); ?>
+				        
+				            <?php get_template_part( 'content-magazine-grid', get_post_format() ); ?>
+				            
+				        <?php endwhile; ?>
+				        
+				</div>
+				
+				        <?php theme_numeric_posts_nav(); ?>
+				        
+				    <?php else : ?>
+				    
+				        <?php get_template_part( 'no-results', 'index' ); ?>
+				        
+				    <?php endif; ?>
+				    
+			</div>		                        
+
+			<?php //get_template_part('partials/full_thin_form'); ?>
+
+		</div><!-- .entry-content -->
+	</div><!-- .page-container -->
+</article><!-- #post-## -->	
+
 	
-</div>
-<?php 
-$description = get_the_archive_description();
-if( ! empty( $description ) ) { ?>
-<div class="masthead_clean_intro">
-	<?php echo '<div class="home_masthead_text wrap archive-description">' . apply_filters( 'tkmulti_the_content', $description ) . '</div>'; ?>
-</div>
-<?php } ?>
-<?php 
-// Build the page
-require get_template_directory() . '/index.php';
- ?>
-<script>
-jQuery(function($) {
-
-	// Auto Padding content top
-	$(window).load(function(){
-		get_header_height();
-	    //function to get current div height
-	    function get_header_height(){
-	        //var footer_height = $('#footer_container').height();
-	        var header_height = $('.block_header #header-container').outerHeight();
-	        topSlider = $("#home_masthead #top-slider .slides");
-	        topSliderImg = $("#home_masthead #top-slider .single-slider-img");
-	        topSlider.css('height', "calc(100vh - " + header_height + "px)");
-	        topSliderImg.css('height', "calc(100vh - " + header_height + "px)");
-	    }
-    });	
-
-});
-</script>  
+<?php get_footer(); ?>
+			
+			
