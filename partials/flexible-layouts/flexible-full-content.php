ca9e05f21ffeef72898282cf065d8582e7f14be8
Tal Katz <tal@tkmedia.co.il>
2019-07-16 22:58:46 +0300

    Update flexible-full-content.php

--- partials/flexible-layouts/flexible-full-content.php
+++ partials/flexible-layouts/flexible-full-content.php
@@ -5,6 +5,7 @@ $one_hide_mobile = get_sub_field('flex_one_col_hide_mobile');
 $one_col_order = get_sub_field('flex_one_col_order');
 $one_col_break = get_sub_field('flex_one_col_break');
 $one_col_block_align = get_sub_field('flex_one_col_block_align');
+$one_col_animation = get_sub_field('flex_one_col_animation');
 
 $one_column_title = get_sub_field('flex_one_col_title');
 $one_column_subtitle = get_sub_field('flex_one_col_subtitle');
@@ -13,16 +14,19 @@ $one_column_subtitle_size = get_sub_field('flex_one_col_subtitle_size');
 $one_column_width = get_sub_field('flex_one_col_width');
 $one_column_title_a = get_sub_field('flex_one_col_title_a');
 $one_column_text = get_sub_field('flex_one_col_text');
-$one_column_btn = get_sub_field('flex_one_col_btn');
-$one_column_link = get_sub_field('flex_one_col_link');
+
 $one_title_color = get_sub_field('flex_one_col_title_color');
 $one_subtitle_color = get_sub_field('flex_one_col_subtitle_color');
 $one_text_color = get_sub_field('flex_one_col_text_color');
 $one_col_btn_color = get_sub_field('flex_one_col_btn_color');
-$one_col_animation = get_sub_field('flex_one_col_animation');
 $one_col_bg_radius = get_sub_field('flex_one_col_bg_radius');
 $one_col_ver_align = get_sub_field('flex_one_col_ver_align');
 
+$one_col_link_type = get_sub_field('flex_one_col_link_type');
+$one_col_page_link = get_sub_field('flex_one_col_page_link');
+$one_column_btn = get_sub_field('flex_one_col_btn');
+$one_column_link = get_sub_field('flex_one_col_link');
+$one_col_form_link = get_sub_field('flex_one_col_form_link');
 $one_col_bg_r = get_sub_field('flex_one_col_bg_r');
 $one_col_bg_l = get_sub_field('flex_one_col_bg_l');
 $one_col_bg_rotate = get_sub_field('flex_one_col_bg_rotate');
@@ -58,13 +62,57 @@ if ( $one_hide_mobile && wp_is_mobile() ) {
 						<?php echo $one_column_text; ?>
 					</div>
 					<?php if( $one_column_btn ) { ?>
-					<a href="<?php echo $one_column_link; ?>" class="<?php echo $one_col_btn_color; ?>">
-					<button class="section_readmore_link watch_btn hoverLink"><?php echo $one_column_btn; ?></button>
+						<?php if( $one_col_link_type == 'page_link' ): ?>
+						<a href="<?php echo $button_page_link; ?>" class="<?php echo $one_col_btn_color; ?>">
+						<?php endif; ?>	
+						<?php if( $one_col_link_type == 'free_link' ): ?>
+						<a href="<?php echo $one_column_link; ?>" class="<?php echo $one_col_btn_color; ?>">
+						<?php endif; ?>
+						<?php if( $one_col_link_type == 'form_link' ): ?>					
+						<a data-fancybox data-src="#popop-form-<?php echo $row;?>-<?php echo $count;?>" href="javascript:;">		
+						<button class="section_readmore_link watch_btn hoverLink"><?php echo $one_column_btn; ?></button>
 					</a>
 					<?php } ?>
 				</div>
 			</div>
 		</div>
+		
+		<?php if( $one_col_link_type == 'form_link' ): ?>
+		<?php 
+		$popup_contact_title = get_option( 'options_default_flex_form_title' );
+		$popup_contact_subtext = get_option( 'options_default_flex_form_subtitle' );
+		?>
+		<div style="display: none;max-width: 700px;" id="popop-form-<?php echo $row;?>-<?php echo $count;?>" class="button-popop-form">
+		
+			<div class="button-popop-form-wrap">
+				
+				<div class="button-popop-form-row">
+					<div class="button-popop-form-col form-image col-xs-12">
+						<?php if( $popup_contact_title ) { ?>
+						<div class="contact-title">
+							<div class="popup_contact_title"><?php echo $popup_contact_title; ?></div>
+						</div>
+						<?php } ?>
+						<?php if( $popup_contact_subtext ) { ?>
+						<div class="contact-title">
+							<div class="popup_contact_subtext"><?php echo $popup_contact_subtext; ?></div>
+						</div>
+						<?php } ?>
+						<div class="contact-form-page">
+							<div class="full_form_id">
+								<div class="full_form_id_wrap">
+									<?php echo do_shortcode( '[contact-form-7 id="'.$one_col_form_link.'" ]' ); ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		
+		</div>
+		<?php endif; ?>
+		
 	</section>
 </div>
 <?php if( $one_col_break ){ ?><div class="break"></div><?php } ?>
