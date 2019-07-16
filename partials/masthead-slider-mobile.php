c08146723134fd34dea9fba88e74cc8f6972e563
Tal Katz <tal@tkmedia.co.il>
2019-07-16 10:30:50 +0300

    Update masthead-slider-mobile.php

--- partials/masthead-slider-mobile.php
+++ partials/masthead-slider-mobile.php
@@ -6,7 +6,7 @@ $title_location = get_post_meta( get_the_ID(), 'mobile_page_masthead_title_locat
 $title_hor = get_post_meta( get_the_ID(), 'mobile_page_masthead_title_hor', true );
 $title_ver = get_post_meta( get_the_ID(), 'mobile_page_masthead_title_ver', true );
 
-$btn_style = get_post_meta( get_the_ID(), 'mobile_page_masthead_btn_style', true );
+$btn_style = get_post_meta( get_the_ID(), 'mobile_page_masthead_button_style', true );
 $btn1_text = get_post_meta( get_the_ID(), 'mobile_page_masthead_btn1_text', true );
 $btn1_link = get_post_meta( get_the_ID(), 'mobile_page_masthead_btn1_link', true );
 $btn2_text = get_post_meta( get_the_ID(), 'mobile_page_masthead_btn2_text', true );
