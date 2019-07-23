22530969445ce8cd09099582cfdc550470c92e5f
Tal Katz <tal@tkmedia.co.il>
2019-07-23 10:03:08 +0300

    Update flexible-page-slider.php

--- partials/flexible-layouts/flexible-page-slider.php
+++ partials/flexible-layouts/flexible-page-slider.php
@@ -24,7 +24,6 @@ $article_slider_source = get_sub_field('flex_article_slider_source');
 $article_slider_latest = get_sub_field('flex_article_slider_latest');
 $article_slider_animation = get_sub_field('flex_article_slider_animation');
 $article_slider_img_size = get_sub_field('flex_article_slider_img_size');
-$hide_date = get_sub_field('flex_artcile_slider_hide_date');
 
 if ( $artcile_slider_hide_mobile && wp_is_mobile() ) {
 //HIDE ON MOBILE
@@ -256,10 +255,8 @@ if ( $artcile_slider_hide_mobile && wp_is_mobile() ) {
 										</div>
 										<div class="page_link_slider_item_title_wrap">
 											<h3 itemprop="name" class="page_link_slider_item_title no-line" style="color:<?php echo $artcile_slider_title_color;?>;justify-content:<?php echo $artcile_slider_title_align;?>;font-size:<?php echo $artcile_slider_title_size;?>px;"><?php the_title(); ?></h3>
-											<?php if ( $hide_date ){ ?>
-											<?php } else { ?>
+											
 											<div class="entry-date"><?php echo get_the_date('d.m.y'); ?></div>
-											<?php } ?>
 										</div>
 									<?php } ?> 
 									</a>
